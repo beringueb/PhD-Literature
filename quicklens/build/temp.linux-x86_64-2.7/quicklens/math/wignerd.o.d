679327cac12117ad823d3741e4fdf5dfa095a26e
Benjamin Beringue <bb510@cam.ac.uk>
2018-04-13 18:03:19 +0100

    Added quicklens

---
+++ quicklens/build/temp.linux-x86_64-2.7/quicklens/math/wignerd.o.d
@@ -0,0 +1,3 @@
+build/temp.linux-x86_64-2.7/quicklens/math/wignerd.o: \
+ quicklens/math/wignerd.c
+commandline: -Ibuild/src.linux-x86_64-2.7/build/src.linux-x86_64-2.7/quicklens/math -I/home/bb510/.local/lib/python2.7/site-packages/numpy/core/include -I/usr/include/python2.7 -c-Ibuild/src.linux-x86_64-2.7/build/src.linux-x86_64-2.7/quicklens/math -I/home/bb510/.local/lib/python2.7/site-packages/numpy/core/include -I/usr/include/python2.7
